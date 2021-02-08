7d9d2aadfde14489ae950c0ecd0da135afedaead
Sushil Ghimire <sushiliconic@gmail.com>
2021-02-08 15:41:48 +0545

    first commit

---
+++ app/src/main/java/com/example/helloword/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.helloword;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+public static final string Tag= MainActivity.classgetSimpleName();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
